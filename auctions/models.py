c97cc1028427284f4897dab3381f61466567a543
amirn003 <amir.naar@gmail.com>
2024-10-02 15:57:22 +0200

    add models in the Auction app

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,19 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Auction(models.Model):
+    name = models.CharField(max_length=64)
+    category = models.CharField(max_length=32)
+    price = models.IntegerField()
+    description = models.TextField()
+    date = models.DateField(auto_created=True)
+
+
+class Bid(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
