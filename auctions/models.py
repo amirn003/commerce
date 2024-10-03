9c922953386a5f52bde390ae92e23f51e3235f4c
amirn003 <amir.naar@gmail.com>
2024-10-03 06:59:04 +0200

    add Comment Model

--- auctions/models.py
+++ auctions/models.py
@@ -26,3 +26,8 @@ class Bid(models.Model):
 
 
 class Comment(models.Model):
+    list = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="auction_list")
+    comment = models.TextField()
+
+    def __str__(self):
+        return f"{self.list}: {self.comment}"
