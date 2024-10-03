929a8fe9ab622d91ae37f698c2ae77eff2d550c1
amirn003 <amir.naar@gmail.com>
2024-10-03 07:07:25 +0200

    update madels

--- auctions/models.py
+++ auctions/models.py
@@ -18,8 +18,9 @@ class Auction(models.Model):
 
 
 class Bid(models.Model):
-    user = models.CharField(max_length=64)
-    offer = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bid_user")
+
+    offer = models.IntegerField(default=1)
 
     def __str__(self):
         return f"{self.user}: {self.offer}"
