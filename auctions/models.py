0092039eb6b340ddac38d1cb77664844b78a0936
amirn003 <amir.naar@gmail.com>
2024-10-05 08:12:21 +0200

    redefine the models and the relational links

--- auctions/models.py
+++ auctions/models.py
@@ -6,7 +6,6 @@ class User(AbstractUser):
     pass
 
 
-
 class Category(models.Model):
     code = models.CharField(max_length=10)
     details = models.CharField(max_length=64)
@@ -17,35 +16,36 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=64)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="product_type")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="category_ref")
     description = models.TextField()
 
     def __str__(self):
         return f"{self.name} ({self.category}): {self.description}"
 
-class Auction(models.Model):
-    name = models.CharField(max_length=64)
-    category = models.CharField(max_length=32)
-    initial_price = models.IntegerField()
-    description = models.TextField()
-    date = models.DateField(auto_created=True)
-
-    def __str__(self):
-        return f"{self.name}: {self.description}"
-
 
 class Bid(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bid_user")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_ref")
+    amount = models.IntegerField()
+    time = models.DateTimeField(auto_now_add=True)
 
-    offer = models.IntegerField(default=1)
 
     def __str__(self):
-        return f"{self.user}: {self.offer}"
+        return f"{self.user}: {self.amount} ({self.time})"
 
 
 class Comment(models.Model):
-    list = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="auction_list")
-    comment = models.TextField()
+    title = models.CharField(max_length=10)
+    description = models.TextField()
 
     def __str__(self):
-        return f"{self.list}: {self.comment}"
+        return f"{self.title}: {self.description}"
+
+
+class AuctionListing(models.Model):
+    bid = models.ForeignKey(Bid, on_delete=models.CASCADE, related_name="bid_ref")
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name="comment_ref")
+    date = models.DateField(auto_created=True)
+
+    def __str__(self):
+        return f"{self.bid}: {self.comment} ({self.date})"
