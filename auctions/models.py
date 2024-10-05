bea37580ff23d4f191070e9b35053083d6287b7d
amirn003 <amir.naar@gmail.com>
2024-10-05 07:57:47 +0200

    crate Product and Category models

--- auctions/models.py
+++ auctions/models.py
@@ -6,6 +6,23 @@ class User(AbstractUser):
     pass
 
 
+
+class Category(models.Model):
+    code = models.CharField(max_length=10)
+    details = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.code}: {self.details}"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=64)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="product_type")
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.name} ({self.category}): {self.description}"
+
 class Auction(models.Model):
     name = models.CharField(max_length=64)
     category = models.CharField(max_length=32)
